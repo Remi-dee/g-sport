248ad30e0bf428d3e2ea87dca9bb1f2438fc2901
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2024-02-16 11:42:05 +0000

    Friday, February 16, 2024 at 12:42 PM West Africa Standard Time

--- app/authentication/forgotPassword/ForgotPassword_comp.jsx
+++ app/authentication/forgotPassword/ForgotPassword_comp.jsx
@@ -9,6 +9,7 @@ function ForgotPassword() {
   const [formData, setFormData] = useState({
     email: "",
   });
+  const [isEmailSent, setIsEmailSent] = useState(false);
 
   const handleChange = (e) => {
     setFormData((prevData) => ({
@@ -30,7 +31,6 @@ function ForgotPassword() {
       console.error("Error sending email:", error);
       return { success: false, error: error.message };
     }
-
   };
 
   return (
@@ -43,62 +43,82 @@ function ForgotPassword() {
                 Forgot Password
               </div>
               <div className=" text-center text-neutral-600 text-base font-normal  leading-tight">
-                Enter your email to reset your password
+                {isEmailSent
+                  ? "Reset your password with the link sent to your email and sign in with your new password"
+                  : "Enter your email to reset your password"}
               </div>
             </div>
 
             <div className="space-y-[32px]">
-              <div className="flex flex-col gap-3 pt-6">
-                <div className="flex flex-col gap-1">
-                  <label
-                    htmlFor="email"
-                    className="block text-sm text-start font-medium text-gray-700"
-                  >
-                    Email Adress
-                  </label>
+              {!isEmailSent && (
+                <>
+                  {" "}
+                  <div className="flex flex-col gap-3 pt-6">
+                    <div className="flex flex-col gap-1">
+                      <label
+                        htmlFor="email"
+                        className="block text-sm text-start font-medium text-gray-700"
+                      >
+                        Email Adress
+                      </label>
 
-                  <div className="w-full   gap-1 inline-flex">
-                    <input
-                      name="email"
-                      required
-                      value={formData.email}
-                      onChange={handleChange}
-                      id="email"
-                      type="text"
-                      autoComplete="on"
-                      className=" text-zinc-400 py-3 px-2   text-base font-normal w-full leading-normal bg-white border border-neutral-300"
-                      placeholder="Enter your email address"
-                    />
+                      <div className="w-full   gap-1 inline-flex">
+                        <input
+                          name="email"
+                          required
+                          value={formData.email}
+                          onChange={handleChange}
+                          id="email"
+                          type="text"
+                          autoComplete="on"
+                          className=" text-zinc-400 py-3 px-2   text-base font-normal w-full leading-normal bg-white border border-neutral-300"
+                          placeholder="Enter your email address"
+                        />
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
+                  <div>
+                    <Button
+                      type="submit"
+                      variant="secondary"
+                      className=" w-full"
+                    >
+                      Continue
+                    </Button>
+                  </div>
+                  <div className="w-80 h-[19px] items-center justify-center  gap-3 inline-flex">
+                    <div className="w-[130px] h-[0px] rotate-180 border border-neutral-300"></div>
+                    <div className="text-black text-base font-normal  leading-tight">
+                      OR
+                    </div>
+                    <div className="w-[130px] h-[0px]  rotate-180 border border-neutral-300"></div>
+                  </div>
+                  <div className="flex justify-center gap-3 border border-spacing-2 mt-[22px] px-6 py-3">
+                    <div className="">
+                      <Image
+                        width={25}
+                        height={25}
+                        alt="Sign in with Google"
+                        src={google}
+                      />
+                    </div>
+                    <div className="text-black text-lg font-normal leading-snug ">
+                      Sign in with Phone
+                    </div>
+                  </div>{" "}
+                </>
+              )}
 
-              <div>
-                <Button type = "submit" variant="secondary" className=" w-full">
-                  Continue
+              {isEmailSent && (
+                <Button
+                  type="button"
+                  onClick={p}
+                  variant="secondary"
+                  className=" w-full"
+                >
+                  {otpSent ? "Verify OTP" : "Send OTP"}
                 </Button>
-              </div>
-
-              <div className="w-80 h-[19px] items-center justify-center  gap-3 inline-flex">
-                <div className="w-[130px] h-[0px] rotate-180 border border-neutral-300"></div>
-                <div className="text-black text-base font-normal  leading-tight">
-                  OR
-                </div>
-                <div className="w-[130px] h-[0px]  rotate-180 border border-neutral-300"></div>
-              </div>
-              <div className="flex justify-center gap-3 border border-spacing-2 mt-[22px] px-6 py-3">
-                <div className="">
-                  <Image
-                    width={25}
-                    height={25}
-                    alt="Sign in with Google"
-                    src={google}
-                  />
-                </div>
-                <div className="text-black text-lg font-normal leading-snug ">
-                  Sign in with Phone
-                </div>
-              </div>
+              )}
             </div>
           </div>
         </div>
