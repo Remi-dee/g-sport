2a498d7b6b4536b662389359256410e6a4cc72a5
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2024-02-19 07:58:44 +0100

    added

--- app/page.js
+++ app/page.js
@@ -1,22 +1,22 @@
 "use client";
 import Image from "next/image";
 import SignUp from "./authentication/signUp/SignUpForm_comp";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { useRouter } from "next/navigation";
 import { appAuth } from "./fireBase/firebase";
 import Authentication from "./authentication/authentication";
 
-
 export default function Home() {
   const router = useRouter();
- 
-
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SignUp />
-      <Authentication />
+      <Suspense>
+        {" "}
+        <SignUp />
+        <Authentication />
+      </Suspense>{" "}
     </main>
   );
 }
