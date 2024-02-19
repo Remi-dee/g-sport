6278eb40553ec8e1acbe671490c0dbfbe34c1459
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2024-02-19 07:23:28 +0100

    added

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 
 import Profile from "../components/Profile_comp";
 import Navigation from "../components/Navbar";
